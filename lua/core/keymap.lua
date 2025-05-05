e1249e3401d0ca52d80b86bf76b0f5e5e26ae87d
tiantang <xu.wenxiang256@outlook.com>
2025-05-06 04:28:25 +0800

    2018

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -2,3 +2,13 @@ vim.g.mapleader = " "
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "gh", "^", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "gl", "$", { noremap = true, silent = true })
+
+local n = true -- this would be your condition variable
+vim.keymap.set("n", "<leader>e", function()
+    if n then
+        vim.cmd("Neotree reveal")
+    else
+        vim.cmd("Neotree close")
+    end
+    n = not n -- toggle the state for next press
+end)
